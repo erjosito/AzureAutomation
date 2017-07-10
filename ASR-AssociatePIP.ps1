e157d25dd30a70bf91ab2aa39cabbc1ed8b92b18
erjosito <erjosito@users.noreply.github.com>
2017-07-10 11:15:45 +0200

    Update ASR-AssociatePIP.ps1

--- ASR-AssociatePIP.ps1
+++ ASR-AssociatePIP.ps1
@@ -3,7 +3,7 @@
         An example runbook which gets all the ARM resources using the Run As Account (Service Principal)
 
     .NOTES
-        AUTHOR: Azure Automation Team
+        AUTHOR: Jose Moreno
         LASTEDIT: Mar 14, 2016
 #>
 
